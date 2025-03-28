3ebbbbb72c906d661ef5bfe8bfb0d2b5bf9d5887
Tetiana Havryliuk <163421073+pterodaCh@users.noreply.github.com>
2025-03-28 08:02:00 +0200

    Update task2.c

--- practice_4/task2.c
+++ practice_4/task2.c
@@ -1,30 +1,31 @@
-#include <stdio. h>
-#include <stdlib. h>
-#include <limits. h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
 
-void test_Malloc_overflow() {
+void test_malloc_overflow() {
   int xa = INT_MAX;
   int xb = INT_MAX;
   int neg_num = -4096;
-  long int num_signed = (long int )xa * xb;
-  size_t num_unsigned = (size_t)xa * xb;
+  long int num_signed = xa * xb;
+  size_t num_unsigned = xa * xb;
   
   printf ("Signed int Multiplication result: %ld\n", num_signed);
   printf ("Unsigned size_t Multiplication result: %zu\n", num_unsigned);
   
-  void *ptr1 = Malloc(num_signed);
+  void *ptr1 = malloc(num_signed);
   printf ("Malloc(%ld) returned: %p\n", num_signed, ptr1);
   
-  void *ptr2 = Malloc(num_unsigned);
+  void *ptr2 = malloc(num_unsigned);
   printf ("Malloc(%zu) returned: %p\n", num_unsigned, ptr2);
 
   void *ptr3 = malloc(neg_num);
-  printf("Malloc(%d) returned %p\n", neg_num, ptr4);
+  printf("Malloc(%d) returned %p\n", neg_num, ptr3);
     
   free (ptr1);
   free(ptr2);
   free(ptr3);
-
+}
 int main() {
   test_Malloc_overflow();
 return 0;
+}
