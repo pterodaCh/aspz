f014434bc537496b3c7437fda2fec911372b81ad
Tetiana Havryliuk <163421073+pterodaCh@users.noreply.github.com>
2025-05-16 01:13:34 +0300

    Create  dices.c

---
+++ practice_3/dices.c
@@ -0,0 +1,16 @@
+                                                                    
+    #include <stdio.h>
+    #include <stdlib.h>
+    #include <time.h>
+
+    int main(int argc, char **argv) {
+        int n, i;
+        n = 6
+        for (i = 0; i < n; i++)
+
+            printf("You rolled %d ", (rand() % 6) + 1);
+
+        return 0;
+
+    }
+
